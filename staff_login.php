3760c11f33cef5f1b4d58557ce2bd9ee2457e148
Davidabishegam <132746389+Davidabishegam@users.noreply.github.com>
2023-05-14 00:10:28 +0800

    Update staff_login.php

--- staff_login.php
+++ staff_login.php
@@ -1,35 +1,61 @@
-<?php
-	include("setup.php");
-	session_start();
-	
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-		$loginuser = mysqli_real_escape_string($conn, $_POST['userid']);
-		$loginpass = mysqli_real_escape_string($conn, $_POST['password']);
-		
-		$sql = "SELECT * FROM staff WHERE staff_loginid = '$loginuser' AND staff_password = '$loginpass'";
-		$result = mysqli_query($conn, $sql);
-		$user = mysqli_fetch_array($result, MYSQLI_ASSOC);
-		
-		$count = mysqli_num_rows($result);
-		
-		if ($count == 1) {
-
-			$_SESSION['login_staff'] = $loginuser;
-			header("location: staff_dashboard.php");
-		} else {
-			header("ID or Password is invalid");
-		}
-	}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            text-align: center;
+        }
+        
+        h1 {
+            color: #333;
+            margin-top: 50px;
+        }
+        
+        form {
+            margin-top: 50px;
+            display: inline-block;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+        
+        label {
+            display: block;
+            margin-bottom: 10px;
+            color: #666;
+            font-weight: bold;
+        }
+        
+        input[type="text"],
+        input[type="password"] {
+            width: 200px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+            margin-bottom: 10px;
+        }
+        
+        input[type="submit"] {
+            background-color: #4CAF50;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+        
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
 </head>
-<body style="text-align:center;">
+<body>
     <h1>Login</h1>
     <form action="" method="post">
         <label for="userid">Staff ID:</label>
