188251c957d913b93ca535d1d9abf4559f151984
Daihuluroll <133075264+Daihuluroll@users.noreply.github.com>
2023-06-16 17:21:23 +0800

    Update display_appointments.php

--- display_appointments.php
+++ display_appointments.php
@@ -5,13 +5,98 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8" >
+  <meta name="description" content="Create new staff" />
+  <meta name="keywords" content="create" />
+  <meta name="author" content="Lim Kwangmin"  />
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <img src="images/logo.png" alt="Our dentistry logo">
     <title>Appointment Table</title>
-    
+	<style>
+	body {
+		font-family: Verdana, Arial sans-serif;
+        background-color: #f2f2f2;
+	}
+			
+	h2   {
+		font-weight: bold;
+	}
+			
+	h3  {
+		text-decoration: underline;
+		color: #333;
+	}
+	
+	th  {
+		font-family: Verdana, Arial sans-serif;
+		color: #333;
+		}
+	
+	img { 
+		height:150px; 
+		width:150px;
+	}
+	
+	 .container {
+            width: 300px;
+            margin-left: 50px;
+            background-color: #f2f2f2;
+            padding: 20px;
+            border-radius: 5px;
+        }
+        
+        .menu {
+            margin-top: 10px;
+            margin-bottom: 20px;
+        }
+        
+        .menu a {
+            display: block;
+            margin-bottom: 10px;
+            padding: 10px;
+            background-color: #f2f2f2;
+            color: #333;
+            text-decoration: none;
+            font-weight: bold;
+            border-radius: 5px;
+        }
+        
+        .menu a:hover {
+            background-color: #ccc;
+        }
+		
+		body {align: top;
+			  margin: 0px;
+			 }
+		
+		table {border: 3px solid #333;
+			   padding: 0px;
+			   margin: 0px;
+			  }
+		
+		th {border: 2px solid #333;
+			padding: 0px;
+			margin: 0px;
+			  }
+			  
+		
+	</style>
+	
 </head>
 <body>
+
+<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:10%">
+        <h3 class="w3-bar-item"> Menu </h3> 
+            <a href="create_appointment.php" class="w3-bar-item w3-button">Create Appointment</a>
+            <a href="display_appointment.php" class="w3-bar-item w3-button">Display Appointment</a>
+            <a href="logout.php" class="w3-bar-item w3-button">Logout</a>
+			
+    </div>
+	<div style="margin-left:12%">
     <h2>Appointment Table</h2>
     <table>
         <thead>
+		
             <tr>
                 <th>Patient ID</th>
                 <th>Patient Name</th>
@@ -50,5 +135,6 @@
             <?php endwhile ?>
         </tbody>
     </table>
+	
 </body>
 </html>
